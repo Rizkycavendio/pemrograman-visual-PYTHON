171c3d328ae31150719cae8b8d4c0ffc27768938
Muhammad Rizky Cavendio <72818583+Rizkycavendio@users.noreply.github.com>
2022-03-23 22:42:10 +0700

    Add files via upload

---
+++ Modul Praktikum 4/NO.2_VolumeTabung.py
@@ -0,0 +1,14 @@
+# Muhammad Rizky Cavendio - 20051397011
+
+class Tabung:
+    def __init__(self, jarijari, tinggi, pi=22/7):
+        self.jarijari=jarijari
+        self.tinggi=tinggi
+        self.pi=pi
+
+    def get_volume(self):
+        return int ((self.pi * (self.jarijari * self.jarijari)) * self.tinggi)
+
+tabung=Tabung(3, 7)
+
+print('Volume tabung adalah:', tabung.get_volume(),'cm^3')
